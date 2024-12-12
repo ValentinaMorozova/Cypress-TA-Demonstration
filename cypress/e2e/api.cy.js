1324af8c2a6f35c5d7678633ddd739df40c3033f
valentina.morozova <valentina.morozova@accenture.com>
2024-12-12 14:23:50 +0200

    parametrize api tests with fixtures

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -3,32 +3,40 @@ describe("API tests", () => {
     Cypress.config("baseUrl", Cypress.env("apiBaseUrl"));
   });
 
-  beforeEach("N", () => {});
-
   it("001. Validate number of results for authors search", () => {
-    const requestURL =
-      Cypress.config("baseUrl") + "/search/authors.json?q=Stephen+King";
-    cy.request({
-      method: "GET",
-      url: requestURL,
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body).to.have.property("numFound", 82);
+    cy.fixture("api_authors_search").then((data) => {
+      data.forEach(({ name, numberOfResults }) => {
+        const requestURL =
+          Cypress.config("baseUrl") +
+          "/search/authors.json?q=" +
+          name.replaceAll(" ", "+");
+        cy.request({
+          method: "GET",
+          url: requestURL,
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+          expect(response.body).to.have.property("numFound", numberOfResults);
+        });
+      });
     });
   });
 
   it("002. Validate first result for book title search", () => {
-    const requestURL =
-      Cypress.config("baseUrl") + "/search.json?q=Harry+Potter&fields=title";
-    cy.request({
-      method: "GET",
-      url: requestURL,
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body["docs"][0]).to.have.property(
-        "title",
-        "Harry Potter and the Philosopher's Stone"
-      );
+    cy.fixture("api_books_titles_search").then((data) => {
+      data.forEach(({ book, title }) => {
+        const requestURL =
+          Cypress.config("baseUrl") +
+          "/search.json?q=" +
+          book.replaceAll(" ", "+") +
+          "&fields=title";
+        cy.request({
+          method: "GET",
+          url: requestURL,
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+          expect(response.body["docs"][0]).to.have.property("title", title);
+        });
+      });
     });
   });
 });
