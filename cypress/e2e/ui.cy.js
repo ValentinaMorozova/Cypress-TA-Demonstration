2645b710e5901915f555830fa96989938418aaf8
valentina.morozova <valentina.morozova@accenture.com>
2024-12-17 15:34:23 +0200

    add fixtures to all ui tests

--- cypress/e2e/ui.cy.js
+++ cypress/e2e/ui.cy.js
@@ -9,153 +9,109 @@ import { shoppingCart } from "../support/page_objects/shoppingCart.js";
 
 describe("UI tests", () => {
   beforeEach("Navigate to the main page", () => {
-    // 1. Open the main page.
     mainPage.navigateToMainPage();
   });
 
-  it("001. Verify user login through the main page", () => {
-    // 2. Click the "Log in" link in the header.
-    mainPage.clickLogIn();
-    loginPage.checkPageIsOpened();
+  // TC-UI-001
+  it("Verify user login through the main page", () => {
+    cy.fixture("ui/001_users.json").then((testData) => {
+      testData.forEach(({ email, password }) => {
+        mainPage.clickLogIn();
+        loginPage.checkPageIsOpened();
 
-    // 3. Enter valid credentials for an existing user:
-    cy.fixture("ui/user.json").then(({ email, password }) => {
-      loginPage.enterEmail(email);
-      loginPage.enterPassword(password);
+        loginPage.enterEmail(email);
+        loginPage.enterPassword(password);
+        loginPage.clickLoginButton();
+        mainPage.checkPageIsOpened();
+
+        mainPage.checkLoggedIn();
+      });
     });
-
-    // 4. Click the"Log in" button
-    loginPage.clickLoginButton();
-    mainPage.checkPageIsOpened();
-
-    // Expected Result
-    // 1.The user should be logged in successfully.
-    mainPage.checkLoggedIn();
   });
 
-  it("002.1. Search computers by different prices ranges", () => {
-    // 2. Enter "comput" in the search panel.
-    mainPage.enterSearchTerm("comput");
+  // TC-UI-002
+  it("Search computers by different prices ranges", () => {
+    cy.fixture("ui/002_searches.json").then((testDatas) => {
+      testDatas.forEach((testData) => {
+        mainPage.enterSearchTerm(testData.searchQuery);
+        mainPage.clickSearch();
+        searchPage.checkPageIsOpened();
+        searchPage.checkNumberOfProducts(testData.numberOfProducts[0]);
 
-    // 3. Click "Search" on the main page.
-    mainPage.clickSearch();
-    searchPage.checkPageIsOpened();
+        searchPage.openAdvancedSearch();
 
-    // Expected Result
-    // 1. There should be 6 products displayed for the initial search after step 3.
-    searchPage.checkNumberOfProducts(6);
+        searchPage.selectCategory(testData.productCategory);
+        searchPage.clickSearchButton();
+        searchPage.checkPageIsOpened();
+        searchPage.checkNumberOfProducts(testData.numberOfProducts[1]);
 
-    // 4. Open "Advanced Search".
-    searchPage.openAdvancedSearch();
+        searchPage.setPriceRange(
+          testData.pricesRange.from,
+          testData.pricesRange.to
+        );
+        searchPage.clickSearchButton();
+        searchPage.checkPageIsOpened();
+        searchPage.checkNumberOfProducts(testData.numberOfProducts[2]);
 
-    // 5. Select "Computers >> Desktops" from the "Category" dropdown.
-    searchPage.selectCategory("Computers >> Desktops");
-
-    // 6. Click "Search" on the search page.
-    searchPage.clickSearchButton();
-    searchPage.checkPageIsOpened();
-
-    // Expected Result
-    // 2. There should be 4 products displayed for the search after step 6.
-    searchPage.checkNumberOfProducts(4);
-
-    // 7. Set the "Price range" to "From 800 to 1500".
-    searchPage.setPriceRange(800, 1500);
-
-    // 8. Click "Search" on the search page.
-    searchPage.clickSearchButton();
-    searchPage.checkPageIsOpened();
-
-    // Expected Result
-    // 3. There should be 3 products displayed for the advanced search after step 7.
-    searchPage.checkNumberOfProducts(3);
-
-    // 4. The first product in the advanced search should have the title "Build your own cheap computer".
-    searchPage.checkFirstProductTitle("Build your own cheap computer");
-  });
-
-  it("003.1. Add default computer to cart", () => {
-    // 2. Navigate to the "Computer" category from the top menu.
-    mainPage.navigateToCategoryFromTopMenu("Computers");
-    categoryPage.checkPageIsOpened("Computers");
-
-    // 3. Select the "Desktops" subcategory.
-    categoryPage.openSubcategory("Desktops");
-    categoryPage.checkPageIsOpened("Desktops");
-
-    // 4. Open the "Build your own cheap computer" product page.
-    categoryPage.openProduct("Build your own cheap computer");
-    productPage.checkPageIsOpened("Build your own cheap computer");
-
-    // 5. Click "Add to cart".
-    productPage.addProductToCart();
-
-    // Expected Result
-    // 1. A success notification bar with text "The product has been added to your shopping cart" should be visible in the header.
-    productPage.checkSuccessBarNotification();
-
-    // 2. The shopping cart should contain 1 product.
-    mainPage.checkShoppingCartNumberOfProducts(1);
-  });
-
-  it("004. Checkout process with logged in user - all default options", () => {
-    // 2. Click the "Log in" link in the header.
-    mainPage.clickLogIn();
-    loginPage.checkPageIsOpened();
-
-    // 3. Enter valid credentials for an existing user:
-    cy.fixture("ui/user.json").then(({ email, password }) => {
-      loginPage.enterEmail(email);
-      loginPage.enterPassword(password);
+        searchPage.checkFirstProductTitle(testData.productTitle);
+      });
     });
+  });
 
-    // 4. Click the"Log in" button
-    loginPage.clickLoginButton();
-    mainPage.checkPageIsOpened();
+  // TC-UI-003
+  it("Add default computer to cart", () => {
+    cy.fixture("ui/003_desktops.json").then((testDatas) => {
+      testDatas.forEach((testData) => {
+        mainPage.navigateToCategoryFromTopMenu(testData.category);
+        categoryPage.checkPageIsOpened(testData.category);
 
-    // 5. Navigate to the "Books" category from the top menu.
-    mainPage.navigateToCategoryFromTopMenu("Books");
-    categoryPage.checkPageIsOpened("Books");
+        categoryPage.openSubcategory(testData.subcategory);
+        categoryPage.checkPageIsOpened(testData.subcategory);
 
-    // 6. Open the "Computing and Internet" product page.
-    categoryPage.openProduct("Computing and Internet");
-    productPage.checkPageIsOpened("Computing and Internet");
+        categoryPage.openProduct(testData.productTitle);
+        productPage.checkPageIsOpened(testData.productTitle);
 
-    // 7. Click "Add to cart".
-    productPage.addProductToCart();
+        productPage.addProductToCart();
+        productPage.checkSuccessBarNotification();
+        mainPage.checkShoppingCartNumberOfProducts(1);
+      });
+    });
+  });
 
-    // 8. Open the shopping cart from the link in the header.
-    mainPage.navigateToShoppingCart();
-    shoppingCart.checkPageIsOpened();
+  // TC-UI-004
+  it("Checkout process with logged in user - all default options", () => {
+    cy.fixture("ui/004_checkout.json").then((testDatas) => {
+      testDatas.forEach((testData) => {
+        mainPage.clickLogIn();
+        loginPage.checkPageIsOpened();
+        loginPage.enterEmail(testData.email);
+        loginPage.enterPassword(testData.password);
+        loginPage.clickLoginButton();
+        mainPage.checkPageIsOpened();
 
-    // 9. Agree with the terms of service by clicking the checkbox.
-    shoppingCart.agreeWithTermsOfService();
+        mainPage.navigateToCategoryFromTopMenu(testData.category);
+        categoryPage.checkPageIsOpened(testData.category);
+        categoryPage.openProduct(testData.productTitle);
+        productPage.checkPageIsOpened(testData.productTitle);
 
-    // 10. Click the "Checkout" button.
-    shoppingCart.navigateToCheckout();
-    checkoutPage.checkPageIsOpened();
+        productPage.addProductToCart();
+        mainPage.navigateToShoppingCart();
+        shoppingCart.checkPageIsOpened();
 
-    // 11. Click "Continue" in the Billing Address block.
-    checkoutPage.clickButtonInBlock("billing");
+        shoppingCart.agreeWithTermsOfService();
+        shoppingCart.navigateToCheckout();
+        checkoutPage.checkPageIsOpened();
 
-    // 12. Click "Continue" in the Shipping Address block.
-    checkoutPage.clickButtonInBlock("shipping");
+        checkoutPage.clickButtonInBlock("billing");
+        checkoutPage.clickButtonInBlock("shipping");
+        checkoutPage.clickButtonInBlock("shipping-method");
+        checkoutPage.clickButtonInBlock("payment-method");
+        checkoutPage.clickButtonInBlock("payment-info");
+        checkoutPage.clickButtonInBlock("confirm-order");
+        completedPage.checkPageIsOpened();
 
-    // 13. Click "Continue" in the Shipping Method block.
-    checkoutPage.clickButtonInBlock("shipping-method");
-
-    // 14. Click "Continue" in the Payment Method block.
-    checkoutPage.clickButtonInBlock("payment-method");
-
-    // 15. Click "Continue" in the Payment Information block.
-    checkoutPage.clickButtonInBlock("payment-info");
-
-    // 16. Click "Confirm" in the Confirm Order block.
-    checkoutPage.clickButtonInBlock("confirm-order");
-    completedPage.checkPageIsOpened();
-
-    // Expected Result
-    // 1. The completed message should contain the text "Your order has been successfully processed!".
-    completedPage.checkSuccessMessage();
+        completedPage.checkSuccessMessage();
+      });
+    });
   });
 });
